9c1141577a3973cdf374fb081a5d1bb339e26b39
kukingkux <cbmamangbae@gmail.com>
2026-01-06 10:36:14 +0700

    Defined UI namespace

---
+++ include/UserInterface.h
@@ -0,0 +1,17 @@
+#ifndef USERINTERFACE_H_INCLUDED
+#define USERINTERFACE_H_INCLUDED
+
+#include <string>
+#include <vector>
+
+namespace UI {
+    void printHeader(const std::string& title);
+    void printDivider();
+    void printMenu(const std::vector<std::string>& options);
+    void printNarration(const std::string& text);
+    void printDialogue(const std::string& speaker, const std::string& text);
+    void printBattleStatus(int playerHP, int enemyHP, const std::string& enemyName);
+    void printSystemMessage(const std::string msg);
+}
+
+#endif
